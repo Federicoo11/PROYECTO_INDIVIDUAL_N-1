e6c0e0a96b167a778f077cb2d8f5379fa5b3ac4b
federico11 <federicooputelli@gmail.com>
2024-07-29 20:43:28 -0300

    Recomendacion

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import pandas as pd
 import calendar
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -6,12 +6,52 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 app = FastAPI()
 
-# Cargar los csv
-df_movies1 = pd.read_csv("csv/movies1.csv")
-df_movies2 = pd.read_csv("csv/movies2.csv")
-df_movies3 = pd.read_csv("csv/movies3.csv")
+# Definir los tipos de datos para optimizar la carga de datos
+dtypes_movies1 = {
+    'title': 'str',
+    'release_date': 'str',
+    'popularity': 'float64',
+    'vote_count': 'int32',
+    'vote_average': 'float64'
+}
 
-def cantidad_filmaciones_mes(mes, df_movies1):
+dtypes_movies2 = {
+    'name': 'str',
+    'title': 'str',
+    'budget': 'float64',
+    'revenue': 'float64'
+}
+
+dtypes_movies3 = {
+    'director': 'str',
+    'title': 'str',
+    'release_date': 'str',
+    'budget': 'float64',
+    'revenue': 'float64'
+}
+
+# Función para cargar datos solo cuando sea necesario
+def load_movies_data(filename, dtypes):
+    return pd.read_csv(filename, dtype=dtypes)
+
+# Función para calcular la matriz de similitud de coseno
+def calculate_cosine_sim(df_movies1):
+    tfidf = TfidfVectorizer(stop_words='english')
+    df_movies1['title'] = df_movies1['title'].fillna('')
+    tfidf_matrix = tfidf.fit_transform(df_movies1['title'])
+    return cosine_similarity(tfidf_matrix, tfidf_matrix), pd.Series(df_movies1.index, index=df_movies1['title']).drop_duplicates()
+
+# Cargar datos y calcular similitud de coseno en el evento de inicio
+cosine_sim, indices = None, None
+
+@app.on_event("startup")
+async def startup_event():
+    global cosine_sim, indices
+    df_movies1 = load_movies_data("csv/movies1.csv", dtypes_movies1)
+    cosine_sim, indices = calculate_cosine_sim(df_movies1)
+    del df_movies1
+
+def cantidad_filmaciones_mes(mes):
     meses_espanol_ingles = {
         "enero": "January",
         "febrero": "February",
@@ -32,19 +72,21 @@ def cantidad_filmaciones_mes(mes, df_movies1):
     if not mes_ingles:
         return f"Mes {mes} no es válido. Por favor ingresa un mes en español."
 
+    df_movies1 = load_movies_data("csv/movies1.csv", dtypes_movies1)
     df_movies1['release_date'] = pd.to_datetime(df_movies1['release_date'], errors='coerce')
     df_filtered = df_movies1[df_movies1['release_date'].dt.month == list(calendar.month_name).index(mes_ingles)]
     cantidad = df_filtered.shape[0]
+    del df_movies1
     
     return f"{cantidad} cantidad de películas fueron estrenadas en el mes de {mes}"
 
 @app.post("/cantidad_filmaciones_mes/")
 def get_cantidad_filmaciones_mes(mes: str):
-    result = cantidad_filmaciones_mes(mes, df_movies1)
+    result = cantidad_filmaciones_mes(mes)
     return {"mensaje": result}
 
 
-def cantidad_filmaciones_dia(dia, df_movies1):
+def cantidad_filmaciones_dia(dia):
     dias_espanol_num = {
         "lunes": 0,
         "martes": 1,
@@ -60,20 +102,23 @@ def cantidad_filmaciones_dia(dia, df_movies1):
     if dia_num is None:
         return f"Día {dia} no es válido. Por favor ingresa un día en español."
     
+    df_movies1 = load_movies_data("csv/movies1.csv", dtypes_movies1)
     df_movies1['release_date'] = pd.to_datetime(df_movies1['release_date'], errors='coerce')
     df_filtered = df_movies1[df_movies1['release_date'].dt.dayofweek == dia_num]
     cantidad = df_filtered.shape[0]
+    del df_movies1
     
     return f"{cantidad} cantidad de películas fueron estrenadas en los días {dia}"
 
 @app.post("/cantidad_filmaciones_dia/")
 def get_cantidad_filmaciones_dia(dia: str):
-    result = cantidad_filmaciones_dia(dia, df_movies1)
+    result = cantidad_filmaciones_dia(dia)
     return {"mensaje": result}
 
 
-def score_titulo(titulo_de_la_filmacion, df):
-    filmacion = df[df['title'].str.lower() == titulo_de_la_filmacion.lower()]
+def score_titulo(titulo_de_la_filmacion):
+    df_movies1 = load_movies_data("csv/movies1.csv", dtypes_movies1)
+    filmacion = df_movies1[df_movies1['title'].str.lower() == titulo_de_la_filmacion.lower()]
 
     if filmacion.empty:
         return f"No se encontró una filmación con el título '{titulo_de_la_filmacion}'."
@@ -81,16 +126,18 @@ def score_titulo(titulo_de_la_filmacion, df):
     titulo = filmacion.iloc[0]['title']
     anio_estreno = pd.to_datetime(filmacion.iloc[0]['release_date']).year
     score = filmacion.iloc[0]['popularity']
+    del df_movies1
 
     return f"La película {titulo} fue estrenada en el año {anio_estreno} con un score/popularidad de {score}"
 
 @app.post("/score_titulo/")
 def get_score_titulo(titulo: str):
-    result = score_titulo(titulo, df_movies1)
+    result = score_titulo(titulo)
     return {"mensaje": result}
 
 
-def votos_titulo(titulo_de_la_filmacion, df_movies1):
+def votos_titulo(titulo_de_la_filmacion):
+    df_movies1 = load_movies_data("csv/movies1.csv", dtypes_movies1)
     filmacion = df_movies1[df_movies1['title'].str.lower() == titulo_de_la_filmacion.lower()]
 
     if filmacion.empty:
@@ -104,15 +151,18 @@ def votos_titulo(titulo_de_la_filmacion, df_movies1):
     if cantidad_votos < 2000:
         return f"La película '{titulo}' no cumple con la condición de tener al menos 2000 valoraciones."
 
+    del df_movies1
+
     return f"La película {titulo} fue estrenada en el año {anio_estreno}. La misma cuenta con un total de {cantidad_votos} valoraciones, con un promedio de {promedio_votos:.2f}"
 
 @app.post("/votos_titulo/")
 def get_votos_titulo(titulo: str):
-    result = votos_titulo(titulo, df_movies1)
+    result = votos_titulo(titulo)
     return {"mensaje": result}
 
 
-def get_actor(nombre_actor, df_movies2):
+def get_actor(nombre_actor):
+    df_movies2 = load_movies_data("csv/movies2.csv", dtypes_movies2)
     peliculas_actor = df_movies2[df_movies2['name'] == nombre_actor]
 
     if peliculas_actor.empty:
@@ -128,27 +178,25 @@ def get_actor(nombre_actor, df_movies2):
     total_retorno = peliculas_actor['return'].sum()
     cantidad_peliculas = len(peliculas_actor)
     promedio_retorno = total_retorno / cantidad_peliculas if cantidad_peliculas > 0 else 0
+    del df_movies2
 
     return f"{nombre_actor} ha participado de {cantidad_peliculas} filmaciones, ha conseguido un retorno de {total_retorno:.2f} con un promedio de {promedio_retorno:.2f} por filmación"
 
 @app.post("/get_actor/")
 def get_actor_endpoint(nombre_actor: str):
-    result = get_actor(nombre_actor, df_movies2)
+    result = get_actor(nombre_actor)
     return {"mensaje": result}
 
 
-def get_director(nombre_director, df_movies3):
+def get_director(nombre_director):
+    df_movies3 = load_movies_data("csv/movies3.csv", dtypes_movies3)
     peliculas_director = df_movies3[df_movies3['director'] == nombre_director].copy()
 
     if peliculas_director.empty:
         return f"No se encontraron películas dirigidas por '{nombre_director}'."
 
-    peliculas_director.loc[:, 'budget'] = pd.to_numeric(peliculas_director['budget'], errors='coerce')
-    peliculas_director.loc[:, 'revenue'] = pd.to_numeric(peliculas_director['revenue'], errors='coerce')
-
-    peliculas_director = peliculas_director.dropna(subset=['budget', 'revenue'])
-
-    peliculas_director.loc[peliculas_director['budget'] == 0, 'budget'] = pd.NA
+    peliculas_director['budget'] = pd.to_numeric(peliculas_director['budget'], errors='coerce')
+    peliculas_director['revenue'] = pd.to_numeric(peliculas_director['revenue'], errors='coerce')
 
     peliculas_director = peliculas_director.dropna(subset=['budget', 'revenue'])
 
@@ -167,6 +215,8 @@ def get_director(nombre_director, df_movies3):
         ganancia = row['revenue']
         peliculas_info.append(f"Película: {titulo}, Fecha de lanzamiento: {fecha_lanzamiento}, Retorno: {retorno_individual:.2f}, Costo: {costo:.2f}, Ganancia: {ganancia:.2f}")
 
+    del df_movies3
+
     mensaje_peliculas = "\n".join(peliculas_info)
     mensaje_final = (f"El director {nombre_director} ha dirigido {cantidad_peliculas} películas, "
                      f"consiguiendo un retorno total de {total_retorno:.2f} con un promedio de {promedio_retorno:.2f} por filmación.\n"
@@ -176,49 +226,29 @@ def get_director(nombre_director, df_movies3):
 
 @app.post("/get_director/")
 def get_director_endpoint(nombre_director: str):
-    result = get_director(nombre_director, df_movies3)
+    result = get_director(nombre_director)
     return {"mensaje": result}
 
-# Usar TF-IDF Vectorizer en los títulos de las películas
-tfidf = TfidfVectorizer(stop_words='english')
-df_movies1['title'] = df_movies1['title'].fillna('')
-tfidf_matrix = tfidf.fit_transform(df_movies1['title'])
-
-# Calcular la similitud del coseno
-cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
-
-# Crear un índice para los títulos de las películas
-indices = pd.Series(df_movies1.index, index=df_movies1['title']).drop_duplicates()
-
+# Función de recomendación
 def recomendacion(titulo):
-    # Verificar si el título existe en los datos
     if titulo not in indices:
         raise HTTPException(status_code=404, detail="Título no encontrado")
-    
-    # Obtener el índice de la película que coincide con el título
+
     idx = indices[titulo]
-
-    # Obtener las puntuaciones de similitud de todas las películas con esa película
     sim_scores = list(enumerate(cosine_sim[idx]))
-
-    # Ordenar las películas según las puntuaciones de similitud
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-
-    # Obtener las puntuaciones de las 5 películas más similares
     sim_scores = sim_scores[1:6]
-
-    # Obtener los índices de las películas más similares
     movie_indices = [i[0] for i in sim_scores]
 
-    # Retornar los títulos de las películas más similares
-    return df_movies1['title'].iloc[movie_indices].tolist()
+    df_movies1 = load_movies_data("csv/movies1.csv", dtypes_movies1)
+    recommendations = df_movies1['title'].iloc[movie_indices].tolist()
+    del df_movies1
 
-# Definir la ruta de recomendación en FastAPI
+    return recommendations
+
 @app.get("/recomendacion/")
 def get_recomendacion(titulo: str):
     return {"recomendaciones": recomendacion(titulo)}
-
-
 
 if __name__ == "__main__":
     import uvicorn
